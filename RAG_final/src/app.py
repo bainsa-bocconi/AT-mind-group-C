0ea44faf463a99ba20e9d89f490e3874f11ca760
Valeria Borgonovo <valeria.vb.borgonovo@gmail.com>
2025-11-21 09:09:54 +0100

    Update app.py

--- RAG_final/src/app.py
+++ RAG_final/src/app.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app = FastAPI(title="Sales RAG (Llama 3.3-3B)")
 
